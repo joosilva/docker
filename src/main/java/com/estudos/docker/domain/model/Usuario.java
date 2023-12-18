628b8e4a085463bb433fe0a903f6e20b7fde84f1
João Victor <joaovictormendessilva1@gmail.com>
2023-12-18 16:38:59 -0300

    Adição das notações de entidade para a model de usuário e nomeação das colunas a partir dos atributos do objeto.

--- src/main/java/com/estudos/docker/domain/model/Usuario.java
+++ src/main/java/com/estudos/docker/domain/model/Usuario.java
@@ -1,8 +1,6 @@
 package com.estudos.docker.domain.model;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
@@ -10,26 +8,33 @@ import lombok.Data;
 import java.time.OffsetDateTime;
 
 @Data
+@Entity(name = "USER")
 public class Usuario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "USER_ID")
     private Long id;
 
     @NotBlank
     @Size(max = 45)
+    @Column(name = "USER_NAME")
     private String nome;
 
     @NotBlank
     @Size(max = 65)
+    @Column(name = "USER_EMAIL")
     private String email;
 
     @NotBlank
     @Size(min = 8, max = 20)
+    @Column(name = "USER_PASSWORD")
     private String senha;
 
+    @Column(name = "USER_CREATED_IN")
     private OffsetDateTime dataCriacao;
 
+    @Column(name = "USER_UPDATED_IN")
     private OffsetDateTime dataAtualizacao;
 
 }
