a9b96f9f9c051e769c9e953c94d4e008687ee11b
João Victor <joaovictormendessilva1@gmail.com>
2023-12-18 14:43:46 -0300

    Início do projeto e instalação das dependências necessárias.

---
+++ src/test/java/com/estudos/docker/DockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudos.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
